af549c032c351161c835daaf26da65b31d5b1964
Luka Bairos <000975328@mohawkcollege.ca>
2025-09-18 23:00:45 -0400

    Replaced an archaic method of subtraction with an assignment operator.

--- ChallengeCode/Program.cs
+++ ChallengeCode/Program.cs
@@ -13,18 +13,18 @@ int goldNumber = int.Parse(playerGold);
 if (goldNumber >= 15)
 {
     Console.WriteLine("Oh yeah, that's perfect. Here, take it, I already stole your gold.");
-    goldNumber = goldNumber - 15;
+    goldNumber -= 15;
     Console.WriteLine($"({playerName} got a Bronze Sword. {playerName}'s pockets feel lighter... {playerName} has {goldNumber} gold left.)");
 }
 
-//Fallback dialogue for if the player enters a negative number
+//Fallback dialogue in case the player enters a negative number
 else if (goldNumber < 0)
 {
     Console.WriteLine("What!? How do you have NEGATIVE GOLD!? This must be some dark magic, I'm getting outta here!");
     Console.WriteLine($"({playerName} scared the goblin off with a paranormal amount of gold. It does not seem like he will come back.)");
 }
 
-//Could I create a fallback for if the player enters a non-integer?
+//I do not know how to create a fallback in case the player enters a non-integer yet
 
 //If the player does not have enough gold, tell them how much more they need
 else
@@ -33,3 +33,5 @@ else
     Console.WriteLine($"Aw c'mon {playerName}, that's not enough gold to feed my goblin family. Come back when you have {requirement} more!");
     Console.WriteLine($"(It seems like the goblin will sell something for 15 gold, but {playerName} only has {goldNumber} gold.)");
 }
+
+int value = 1;
